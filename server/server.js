2379f9b75c38c19427f6bcc41e8445ae01029e08
kundankumar123512 <kumarkundan123512@gmail.com>
2025-10-05 00:38:16 +0530

    update

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://ecommerce-cysz.vercel.app",
+    origin: process.env.FRONTEND_URL || "https://ecommerce-cysz.vercel.app/auth/login",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
